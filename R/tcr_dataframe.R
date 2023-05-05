387d1fa393a3ca82866d242f369e2703d572f3b9
Qile <qile0317@gmail.com>
2023-05-05 18:23:30 +0200

    initial commit

---
+++ R/tcr_dataframe.R
@@ -0,0 +1,9 @@
+#' all_contig_annotations
+#'
+#' A `data.frame` object of a read-in csv of the `all_contig_annotations.csv` file from 10X's single cell immune profiling.
+#'
+#' @source <https://www.10xgenomics.com/resources/datasets/pbm-cs-from-bal-bc-mice-tcr-enrichment-from-amplified-c-dna-1-standard-3-0-0>
+#' @format data.frame
+#' @examples
+#'   tcr_dataframe
+"tcr_dataframe"
---
+++ tests/testthat/test-main.R
@@ -0,0 +1,3 @@
+test_that("tcr_dataframe works", {
+  expect_equal(2 * 2, 4)
+})

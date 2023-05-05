f4bc9a1b2772f645e46e06b0c78713bcbb3b38c5
Qile <qile0317@gmail.com>
2023-05-05 20:29:47 +0200

    wrote tests

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,3 +1,19 @@
 test_that("tcr_dataframe works", {
-  expect_equal(2 * 2, 4)
+  data("tcr_dataframe")
+  expect_length(tcr_dataframe, 18)
+  expect_identical(
+    colnames(tcr_dataframe),
+    c("barcode", "is_cell", "contig_id", "high_confidence", "length", 
+      "chain", "v_gene", "d_gene", "j_gene", "c_gene", "full_length", 
+      "productive", "cdr3", "cdr3_nt", "reads", "umis", "raw_clonotype_id", 
+      "raw_consensus_id")
+  )
+  expect_length(rownames(tcr_dataframe), 6812)
+  expect_identical(tcr_dataframe[[6]][420], "Multi") # random test
+})
+
+test_that("pbmc works", {
+  data("pbmc")
+  expect_identical(class(pbmc), structure("Seurat", package = "SeuratObject"))
+  expect_length(pbmc@meta.data, 5)
 })
